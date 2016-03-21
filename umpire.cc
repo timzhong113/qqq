148f2b2dbb555f7461e3aea33e95d5906841d00c
Joanna Chow <Joanna_jiayi@shuutekiMacBook-Pro.local>
2016-03-20 22:20:00 -0400

    not done yet with umpire

--- umpire.cc
+++ umpire.cc
@@ -15,53 +15,53 @@ void Umpire::start(){
 				uint32_t size = (uint32_t)players.size()-1;
 				uint32_t num = prng(size);
 				unsigned int playerId = (unsigned int)num;
-				prt.print(2,6,playerId);
+				prt.print(Printer::Umpire,6,playerId);
 
 				unsigned int length = players.size();
 				unsigned int position;
 				for(unsigned int i=0; i<length; i++){
-					if(players[i].id == playerId){
+					if(players[i]->getId() == playerId){
 						position = i;
 						break;
 					}
 				}
 
 				isEven = false;
-				players[position].toss(mashed);
+				players[position]->toss(mashed);
 			}
 			else{	//throw fried potato
 				fried.reset();
 				uint32_t size = (uint32_t)players.size()-1;
 				uint32_t num = prng(size);
 				unsigned int playerId = (unsigned int)num;
-				prt.print(2,6,playerId);
+				prt.print(Printer::Umpire,6,playerId);
 
 				unsigned int length = players.size();
 				unsigned int position;
 				for(unsigned int i=0; i<length; i++){
-					if(players[i].id == playerId){
+					if(players[i]->getId() == playerId){
 						position = i;
 						break;
 					}
 				}
 
 				isEven = true;
-				players[position].toss(fried);
+				players[position]->toss(fried);
 			}
-		}catch(const Lost &id){
+		}catch(Player::Lost id){
 			unsigned int length = players.size();
 			unsigned int position;
 			for(unsigned int i=0; i<length; i++){
-				if(players[i].id == id){
+				if(players[i]->getId() == id){
 					position = i;
 					break;
 				}
 			}
-			prt.print(3,2,0,id);
+			prt.print(Printer::Player,2,0,id);
 			players.erase(players.begin()+position);
 			if(players.size()==1){	//someone wins
-				unsigned int winner = players[0].id;
-				prt.print(3,3,0,winner);
+				unsigned int winner = players[0]->getId();
+				prt.print(Printer::Player,3,0,winner);
 				exit(0);
 			}
 			else continue;
